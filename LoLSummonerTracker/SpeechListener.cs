b735571ed7ce2309b37d4e933b30fe96294dddc3
Henrik Westöö <henrikwestoo@hotmail.com>
2020-05-11 17:02:58 +0200

    Voice recognition is now contiuous

--- LoLSummonerTracker/SpeechListener.cs
+++ LoLSummonerTracker/SpeechListener.cs
@@ -15,32 +15,32 @@ namespace LoLSummonerTracker
             gui.setLabel("speech activated");
 
             // Create an in-process speech recognizer for the en-US locale.  
-            using (
+
+
             SpeechRecognitionEngine recognizer =
               new SpeechRecognitionEngine(
-                new System.Globalization.CultureInfo("en-US")))
-            {
-
+                new System.Globalization.CultureInfo("en-US"));
+            
                 // Create and load a dictation grammar.  
                 recognizer.LoadGrammar(new DictationGrammar());
 
                 // Add a handler for the speech recognized event.  
                 recognizer.SpeechRecognized +=
-                  new EventHandler<SpeechRecognizedEventArgs>(recognizer_SpeechRecognized);
+                  new EventHandler<SpeechRecognizedEventArgs>(Recognizer_SpeechRecognized);
 
                 // Configure input to the speech recognizer.  
                 recognizer.SetInputToDefaultAudioDevice();
-                
+
                 // Start asynchronous, continuous speech recognition.  
                 recognizer.RecognizeAsync(RecognizeMode.Multiple);
-                
-            }
+
+            
 
 
         }
 
         // Handle the SpeechRecognized event.  
-        public void recognizer_SpeechRecognized(object sender, SpeechRecognizedEventArgs e)
+        public void Recognizer_SpeechRecognized(object sender, SpeechRecognizedEventArgs e)
         {
             string hej = "hej";
             string hej2 = "2hej";
